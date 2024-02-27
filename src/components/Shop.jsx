c519fa76dc78df1e7ce60e23d893fcfa85d1ffd4
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-02-27 11:20:03 +0530

    ffb

---
+++ src/components/Shop.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import Button from './Button';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+function Shop({ data,ref }) {
+    gsap.registerPlugin(ScrollTrigger);
+    const container = useRef();
+    useGSAP(() => {
+        // ✅ safe, created during execution, selector text scoped
+     
+      });
+
+  return (
+    <div ref={ref} className='w-[25vw] max-md:w-full  p-2 flex flex-col overflow-hidden items-center gap-5 shop rounded-md'>
+      <div className='flex items-center justify-center p-10'>
+        <img src={data.url} alt="" />
+      </div>
+      <div className='text-center flex flex-col overflow-hidden gap-5'>
+        <h1 className='text-3xl sh text-[#ffffff] '>{data.title}</h1>
+        <p className='text-2xl sh text-[#ffffff]'>${data.money} / <span className='text-xs'>month</span></p>
+        {data.list.map((item) => (
+          <span className='sh' key={item}>{item}</span>
+        ))}
+      </div>
+      <div className=''>
+        <Button title='more info' link={`/checkout?id=${data.id}`} />
+      </div>
+    </div>
+  );
+}
+
+export default Shop;
